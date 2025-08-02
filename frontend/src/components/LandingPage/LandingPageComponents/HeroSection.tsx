3ac4f832c9a9ca2b51d445274187d13eb15d6fb0
Sid <siddhantg2002@gmail.com>
2025-08-02 23:40:59 +0530

    feat(frontend): implement new functionality

--- frontend/src/components/LandingPage/LandingPageComponents/HeroSection.tsx
+++ frontend/src/components/LandingPage/LandingPageComponents/HeroSection.tsx
@@ -11,7 +11,7 @@ function HeroSection() {
   const [copied, setCopied] = useState(false);
   
   const handleCopy = () => {
-    navigator.clipboard.writeText("npx optimac");
+    navigator.clipboard.writeText("npm install -g optimac");
     setCopied(true);
     setTimeout(() => setCopied(false), 2000);
   };

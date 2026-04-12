cceb8c38c2f470817214a4a44b0069b1a227d7c1
Sid. <siddhantg2002@gmail.com>
2026-04-12 12:56:11 +0530

    fix(docs): resolve issues and bugs

--- frontend/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { RouterProvider } from "react-router-dom";
-import routes from "./Routes";
-
-function App() {
-  return <RouterProvider router={routes} />;
-}
-
-export default App;
--- frontend/src/Routes/Home/index.tsx
+++
@@ -1,7 +0,0 @@
-import LandingPage from "@/components/LandingPage/LandingPageLayout/LandingPage";
-
-const Home = () => {
-  return <LandingPage />;
-};
-
-export default Home;
